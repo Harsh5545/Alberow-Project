5bb6b4d8ed6d7cbf84ea311afd53042d0b2fbe97
Harsh <harshadkajale333@gmail.com>
2025-06-28 22:59:43 +0530

    add

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,7 +8,7 @@ import { motion, AnimatePresence } from "framer-motion"
 import { cn } from "@/lib/utils"
 
 const navLinks = [
-  { name: "Home", href: "#home" },
+  { name: "Home", href: "/" },
   { name: "About", href: "about" },
   { name: "Services", href: "services" },
   { name: "Projects", href: "projects" },
