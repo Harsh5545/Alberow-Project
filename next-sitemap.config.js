63554a6f336b61f68cfb7844f71a8e22506e33dd
Harsh <harshadkajale333@gmail.com>
2025-07-20 12:08:21 +0530

    updatea

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://alberow.com',
-  generateRobotsTxt: true, // also generates robots.txt
-}
+  generateRobotsTxt: true,        // <- generate robots.txt
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 7000,
+};
